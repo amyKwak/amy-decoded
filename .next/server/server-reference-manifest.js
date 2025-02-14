4a8e54c47633970323c4cd84482a046a20bf1184
Amy Kwak <yoomikwak@gmail.com>
2025-02-13 23:01:17 -0800

    feat: next

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xwQeYMIZeqYTR0x4XVUOxWHbFi0h66j3bAwZAvgrIE0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
