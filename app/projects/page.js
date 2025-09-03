a3a068b304cd2f8dac8ce5c199aba567b68a5485
Amy Kwak <yoomikwak@gmail.com>
2025-09-03 15:55:46 -0700

    projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,5 +1,31 @@
+"use client";
+
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div className="page">
+      <a href="/projects/pillpal" className="link">
+        PillPal
+      </a>
+      <style jsx>{`
+        .page {
+          max-width: 500px;
+          min-height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin: 0 auto;
+        }
+        .link {
+          font-weight: bold;
+          color: var(--color-text);
+          font-size: 2rem;
+        }
+        .link:hover {
+          color: var(--color-primary);
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Projects;
